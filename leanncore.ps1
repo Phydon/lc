fd7d7675b2e1f6f6b7a6f49af876bdfcfcfb7d31
Phydon <leann.phydon@gmail.com>
2024-04-15 22:49:38 +0200

    check current version of lc-tools ->only update outdated ones

--- leanncore.ps1
+++ leanncore.ps1
@@ -9,7 +9,7 @@ $owner = [Runtime.InteropServices.Marshal]::PtrToStringAuto(
 Write-Host "Downloading leanncore-utils into $destination_path..."
 
 # create output directory if it doesn't exist
-If(!(test-path -PathType container $destination_path)) {
+If (!(test-path -PathType container $destination_path)) {
 	[void](New-Item -ItemType Directory -Path $destination_path)
 }
 
@@ -18,13 +18,13 @@ $utils = @("sf", "mg", "pf", "cx", "sl", "up", "witchfile", "map", "gib", "ms", 
 try {
 	$counter = 0
 	foreach ($util in $utils) {
-		$progress = [math]::round(($counter/$utils.count) * 100, 2)
+		$progress = [math]::round(($counter/$utils.count) * 100, 0)
 		
 		Write-Progress -Activity "Downloading" -Status "$progress% Complete:" -CurrentOperation "$util.exe" -PercentComplete $progress
 		
 		# supress the progress bar
 		$ProgressPreference = "SilentlyContinue"
-	
+
 		# get latest release version for each leanncore util
 		$latestRelease = Invoke-WebRequest "https://github.com/$owner/$util/releases/latest" -Headers @{"Accept"="application/json"} -ErrorAction Stop
 		$json = $latestRelease.Content | ConvertFrom-Json
@@ -32,9 +32,23 @@ try {
 		$url = "https://github.com/$owner/$util/releases/download/$latestVersion/$util.exe"
 
 		$outfile = "$destination_path/$util.exe"
+		
+		# get current version for each leanncore util
+		$version = (Invoke-Expression "$outfile -V").split(" ")[-1]
+		# TODO do this in one line
+		$versionV = "V$version"
 
-		Invoke-WebRequest -Uri $url -Outfile $outfile -ErrorAction Stop
+		# compare current version with latest release version
+		If (($versionV -eq $latestVersion)) {
+			Write-Host "$util $versionV already up to date"
+		} Else {
+			Write-Host "Updating $util $versionV => $latestVersion"
 
+			# FIXME doesn`t update an existing file
+			# FIXME e.g.: mg V1.2.5 doesn't get updated to V1.2.6 if mg.exe already exists in destination_path
+			Invoke-WebRequest -Uri $url -Outfile $outfile -ErrorAction Stop 
+		}
+		
 		# restore the progress bar
 		$ProgressPreference = "Continue"
 		
