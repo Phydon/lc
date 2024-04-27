bb67c546ee4983f5f714951ec7a3be541533d300
Phydon <leann.phydon@gmail.com>
2024-04-27 11:43:01 +0200

    added version flag

--- leanncore.ps1
+++ leanncore.ps1
@@ -1,72 +1,85 @@
-$destination_path = "~/.local/bin"
-
-# get repo-owner from user (don't show on screen)
-$input = Read-Host "Enter owner: " -AsSecureString
-$owner = [Runtime.InteropServices.Marshal]::PtrToStringAuto(
-	[Runtime.InteropServices.Marshal]::SecureStringToBSTR($input)
+param(
+  [switch]$V
 )
 
-Write-Host "Downloading leanncore-utils into $destination_path"
+# Define the version
+$scriptVersion = "lc 1.0.0"
 
-# create output directory if it doesn't exist
-If (!(test-path -PathType container $destination_path)) {
-	[void](New-Item -ItemType Directory -Path $destination_path)
-}
+# Check if version argument was passed
+if ($V) {
 
-$utils = @("sf", "mg", "pf", "cx", "sl", "up", "witchfile", "map", "gib", "ms", "xa", "gerf")
+  # Show the version
+  Write-Host "$scriptVersion"
 
-try {
-	$counter = 0
-	foreach ($util in $utils) {
-		$progress = [math]::round(($counter/$utils.count) * 100, 0)
-		
-		Write-Progress -Activity "Downloading" -Status "$progress% Complete:" -CurrentOperation "$util.exe" -PercentComplete $progress
-		
-		# supress the progress bar
-		$ProgressPreference = "SilentlyContinue"
+} else {
+	$destination_path = "~/.local/bin"
 
-		# get latest release version for each leanncore util
-		$latestRelease = Invoke-WebRequest "https://github.com/$owner/$util/releases/latest" -Headers @{"Accept"="application/json"} -ErrorAction Stop
-		$json = $latestRelease.Content | ConvertFrom-Json
-		$latestVersion = $json.tag_name
-		$url = "https://github.com/$owner/$util/releases/download/$latestVersion/$util.exe"
-
-		$outfile = "$destination_path/$util.exe"
-		
-		If (Test-Path $outfile) {
-			# get current version for each leanncore util if it already exists in destination_path
-			$currentVersion = "V$((Invoke-Expression "$outfile -V").split(" ")[-1])"
-			
-			# compare current version with latest release version
-			# only download util when currentVersion != latestVersion
-			# TODO make semantic version check
-			# TODO update when currentVersion < latestVersion
-			If (!($currentVersion -eq $latestVersion)) {
-				Write-Host "Updating $util $currentVersion => $latestVersion"
-				Invoke-WebRequest -Uri $url -Outfile $outfile -ErrorAction Stop 
-			# } Else {
-			# 	# TODO remove else statement??
-			# 	Write-Host "$util $currentVersion already up to date"
-			}
-		} Else {
-			# if util doesn't already exist in destination_path
-			Write-Host "Downloading $util $latestVersion"
-			Invoke-WebRequest -Uri $url -Outfile $outfile -ErrorAction Stop 
-		}
-
-		
-		# restore the progress bar
-		$ProgressPreference = "Continue"
-		
-		$counter++
+	# create output directory if it doesn't exist
+	if (!(Test-Path -PathType container $destination_path)) {
+		[void](New-Item -ItemType Directory -Path $destination_path)
 	}
 
-	Write-Host "All done"
-}
-catch {
-	Write-Warning $Error[0]
-}
-finally {
-	# restore the progress bar
-	$ProgressPreference = "Continue"
+	# get repo-owner from user (don't show on screen)
+	$input = Read-Host "Enter owner: " -AsSecureString
+	$owner = [Runtime.InteropServices.Marshal]::PtrToStringAuto(
+		[Runtime.InteropServices.Marshal]::SecureStringToBSTR($input)
+	)
+
+	$utils = @("sf", "mg", "pf", "cx", "sl", "up", "witchfile", "map", "gib", "ms", "xa", "gerf")
+
+	try {
+		Write-Host "Downloading leanncore-utils into $destination_path"
+
+		$counter = 0
+		foreach ($util in $utils) {
+			$progress = [math]::round(($counter/$utils.count) * 100, 0)
+	
+			Write-Progress -Activity "Downloading" -Status "$progress% Complete:" -CurrentOperation "$util.exe" -PercentComplete $progress
+	
+			# supress the progress bar
+			$ProgressPreference = "SilentlyContinue"
+
+			# get latest release version for each leanncore util
+			$latestRelease = Invoke-WebRequest "https://github.com/$owner/$util/releases/latest" -Headers @{"Accept"="application/json"} -ErrorAction Stop
+			$json = $latestRelease.Content | ConvertFrom-Json
+			$latestVersion = $json.tag_name
+			$url = "https://github.com/$owner/$util/releases/download/$latestVersion/$util.exe"
+
+			$outfile = "$destination_path/$util.exe"
+	
+			If (Test-Path $outfile) {
+				# get current version for each leanncore util if it already exists in destination_path
+				$currentVersion = "V$((Invoke-Expression "$outfile -V").split(" ")[-1])"
+		
+				# compare current version with latest release version
+				# only download util when currentVersion != latestVersion
+				# TODO make semantic version check
+				# TODO update when currentVersion < latestVersion
+				If (!($currentVersion -eq $latestVersion)) {
+					Write-Host "Updating $util $currentVersion => $latestVersion"
+					Invoke-WebRequest -Uri $url -Outfile $outfile -ErrorAction Stop 
+				}
+			} Else {
+				# if util doesn't already exist in destination_path
+				Write-Host "Downloading $util $latestVersion"
+				Invoke-WebRequest -Uri $url -Outfile $outfile -ErrorAction Stop 
+			}
+
+	
+			# restore the progress bar
+			$ProgressPreference = "Continue"
+	
+			$counter++
+		}
+
+		Write-Host "All done"
+	}
+	catch {
+		Write-Warning $Error[0]
+		Write-Error $_
+	}
+	finally {
+		# restore the progress bar
+		$ProgressPreference = "Continue"
+	}
 }
